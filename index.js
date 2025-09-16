126b0d0889aecbffe25ec29e2a7f7c6e271f8831
Phatogram <todo_xqteuiero@outlook.com>
2025-09-16 12:59:46 -0500

    fix(main):"fix commit "

--- index.js
+++
@@ -1,6 +0,0 @@
-// Captura el formulario de contacto
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Gracias por contactarnos, te responderemos pronto 😊");
-  this.reset();
-});
